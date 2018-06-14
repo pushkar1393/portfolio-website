b7f627168751e166e664edabab7cd7882c405736
Pushkar Khedekar <pushkarkhedekar@gmail.com>
2018-06-14 17:05:04 -0400

    added favicons

--- js/portfolio_scripts.js
+++ js/portfolio_scripts.js
@@ -41,14 +41,13 @@
   });
 
   // To implement animated charts
-  $('.chart').easyPieChart({
-    barColor:'#fff',
-    trackColor:'rgba(255,255,255,0.2)',
-    lineCap: 'butt',
-    lineWidth:'6',
-    size: '120'
-  });
-
+    $('.chart').easyPieChart({
+      barColor:'#fff',
+      trackColor:'rgba(255,255,255,0.2)',
+      lineCap: 'butt',
+      lineWidth:'6',
+      size: '120'
+    });
 
   // form submission event
   $("#contactMe").submit(function(e){
@@ -56,7 +55,7 @@
     submitForm();
   });
 
-// To call php mail service
+  // To call php mail service
   function submitForm(){
     var name = $("#contact_name").val().trim();
     var email = $("#contact_email").val().trim();
@@ -69,77 +68,77 @@
       data: "name=" + name + "&email=" + email + "&phone=" + phone + "&message=" + message,
       success : function(text){
         if (text == "success"){
-        $("#confirmationModal").modal('show');
-        $("#contact_name, #contact_email,#contact_number,#contact_message").val("");
+          $("#confirmationModal").modal('show');
+          $("#contact_name, #contact_email,#contact_number,#contact_message").val("");
         }
       }
     });
   }
 
 
-// To setup modal for project details
-$(document.body).on('click','.portfolio-item',function(e){
-  e.preventDefault();
-  var id = this.id;
-  var p = getProjectById(id);
-  $(".modal-title").html(p.title);
-  $(".pop-caption").html(p.caption);
-  $(".pop-description").html(p.description);
-  if(p.tech.length != 0){
-    var tech = '<h6><strong>Tech Used: </strong></h6><ul>';
-    for(var i =0; i < p.tech.length; i++){
-      tech += '<li>'+p.tech[i]+'</li>';
+  // To setup modal for project details
+  $(document.body).on('click','.portfolio-item',function(e){
+    e.preventDefault();
+    var id = this.id;
+    var p = getProjectById(id);
+    $(".modal-title").html(p.title);
+    $(".pop-caption").html(p.caption);
+    $(".pop-description").html(p.description);
+    if(p.tech.length != 0){
+      var tech = '<h6><strong>Tech Used: </strong></h6><ul>';
+      for(var i =0; i < p.tech.length; i++){
+        tech += '<li>'+p.tech[i]+'</li>';
+      }
+      tech +='</ul>';
+      $(".pop-list").html(tech);
     }
-    tech +='</ul>';
-    $(".pop-list").html(tech);
-  }
-  if(p.link !== ""){
-    $(".pop-link").html('<h6><strong>Project Link: </strong></h6><a class="btn btn-dark" href='+p.link+' target="_blank" ><i class="icon-link mx-2"></i>'+p.title+'</a>');
-  }
-  $("#projectDetailModal").modal("show");
-});
-
-//To clear project modal on close
-$(document.body).on('click','.btn-clear',function(){
-
-  $(".modal-title,.pop-caption,.pop-description,.pop-list,.pop-link").html("");
-});
-
-//To call setupPortfolio on page load
-$(document).ready(function() {
-  setUpPortfolio();
-});
-
-//To setup Portfolio section on page load
-function setUpPortfolio(){
-  $.ajax({
-    dataType: "json",
-    url: "/assets/project-data.json",
-    success: function(data){
-      sessionStorage.setItem('projects',JSON.stringify(data.projects));
-      var projectList = $.parseJSON(sessionStorage.getItem('projects'));
-      var markUp = '';
-      for (var i=0; i < projectList.length; i++){
-      markUp += '<div class="col-lg-6">'+
-      '<a class="portfolio-item" id="'+ projectList[i].id+'" href="#">'+
-      '<span class="caption"><span class="caption-content">'+
-      '<h2>'+projectList[i].title+'</h2>'+
-      '<p class="mb-0">'+projectList[i].caption+'</p>'+
-      '</span>'+
-      '</span>'+
-      '<img class="img-fluid" src="'+projectList[i].image+'" alt="">'+
-      '</a>'+
-      '</div>';
+    if(p.link !== ""){
+      $(".pop-link").html('<h6><strong>Project Link: </strong></h6><a class="btn btn-dark" href='+p.link+' target="_blank" ><i class="icon-link mx-2"></i>'+p.title+'</a>');
     }
-    $(".block-project").html(markUp);
-    }
+    $("#projectDetailModal").modal("show");
   });
-}
 
-// To retrieve project from sessionStorage
-function getProjectById(id){
-  var projectList = $.parseJSON(sessionStorage.getItem('projects'));
-  return projectList[id];
-}
+  //To clear project modal on close
+  $(document.body).on('click','.btn-clear',function(){
+
+    $(".modal-title,.pop-caption,.pop-description,.pop-list,.pop-link").html("");
+  });
+
+  //To call setupPortfolio on page load
+  $(document).ready(function() {
+    setUpPortfolio();
+  });
+
+  //To setup Portfolio section on page load
+  function setUpPortfolio(){
+    $.ajax({
+      dataType: "json",
+      url: "/assets/project-data.json",
+      success: function(data){
+        sessionStorage.setItem('projects',JSON.stringify(data.projects));
+        var projectList = $.parseJSON(sessionStorage.getItem('projects'));
+        var markUp = '';
+        for (var i=0; i < projectList.length; i++){
+          markUp += '<div class="col-lg-6">'+
+          '<a class="portfolio-item" id="'+ projectList[i].id+'" href="#">'+
+          '<span class="caption"><span class="caption-content">'+
+          '<h2>'+projectList[i].title+'</h2>'+
+          '<p class="mb-0">'+projectList[i].caption+'</p>'+
+          '</span>'+
+          '</span>'+
+          '<img class="img-fluid" src="'+projectList[i].image+'" alt="">'+
+          '</a>'+
+          '</div>';
+        }
+        $(".block-project").html(markUp);
+      }
+    });
+  }
+
+  // To retrieve project from sessionStorage
+  function getProjectById(id){
+    var projectList = $.parseJSON(sessionStorage.getItem('projects'));
+    return projectList[id];
+  }
 
 })(jQuery); // End of use strict
